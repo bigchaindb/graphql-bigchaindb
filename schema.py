b9763df42ed5b5845fcd0fa7edb1eaf873f55fec
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-08-02 12:08:11 +0200

    added fulfills type

--- schema.py
+++ schema.py
@@ -13,6 +13,10 @@ class OutputType(graphene.ObjectType):
     public_keys = graphene.List(graphene.String)
     amount = graphene.String()
 
+    @classmethod
+    def from_json(cls, output):
+        return cls(**output)
+
 
 class InputType(graphene.ObjectType):
     name = 'Input'
@@ -20,7 +24,16 @@ class InputType(graphene.ObjectType):
 
     owners_before = graphene.List(graphene.String)
     fulfillment = graphene.String()
-    fulfills = GenericScalar()
+    fulfills = graphene.Field(lambda: FulfillsType)
+
+    @classmethod
+    def from_json(cls, input_):
+        fulfills = FulfillsType.from_json(input_['fulfills'])
+        return cls(
+            owners_before=input_['owners_before'],
+            fulfillment=input_['fulfillment'],
+            fulfills=fulfills
+        )
 
 
 class TransactionType(graphene.ObjectType):
@@ -36,18 +49,47 @@ class TransactionType(graphene.ObjectType):
     outputs = graphene.List(OutputType)
 
     @classmethod
-    def from_retrieved_tx(cls, retrieved_tx):
-        outputs = [OutputType(**output) for output in retrieved_tx['outputs']]
-        inputs = [InputType(**input) for input in retrieved_tx['inputs']]
+    def from_json(cls, retrieved_tx):
+        outputs = [
+            OutputType.from_json(output) for output in retrieved_tx['outputs']
+        ]
+        inputs = [
+            InputType.from_json(input_) for input_ in retrieved_tx['inputs']
+        ]
 
         return cls(
             id=retrieved_tx['id'],
             version=retrieved_tx['version'],
             inputs=inputs,
             outputs=outputs,
+            operation=retrieved_tx['operation'],
             asset=retrieved_tx['asset'],
             metadata=retrieved_tx['metadata'],
         )
+
+
+class FulfillsType(graphene.ObjectType):
+    name = 'Fulfills'
+    description = '...'
+
+    output_index = graphene.Int()
+    transaction = graphene.Field(TransactionType)
+
+    @classmethod
+    def from_json(cls, fulfills):
+        bdb = BigchainDB()
+        if fulfills:
+            retrieved_tx = bdb.transactions.retrieve(fulfills['transaction_id'])
+
+            return cls(
+                output_index=fulfills['output_index'],
+                transaction=TransactionType.from_json(retrieved_tx)
+            )
+        else:
+            return cls(
+                output_index=None,
+                transaction=None
+            )
 
 
 class QueryType(graphene.ObjectType):
@@ -68,13 +110,13 @@ class QueryType(graphene.ObjectType):
         bdb = BigchainDB()
         txid = args.get('id')
         retrieved_tx = bdb.transactions.retrieve(txid)
-        return TransactionType.from_retrieved_tx(retrieved_tx)
+        return TransactionType.from_json(retrieved_tx)
 
     def resolve_transactions(self, args, context, info):
         bdb = BigchainDB()
         asset_id = args.get('asset_id')
         retrieved_txs = bdb.transactions.get(asset_id=asset_id)
-        return [TransactionType.from_retrieved_tx(tx) for tx in retrieved_txs]
+        return [TransactionType.from_json(tx) for tx in retrieved_txs]
 
 
 schema = graphene.Schema(
